b43369589471e0af7b0a4a364dddcf87dd0cd629
Olaf Alders <olaf@wundersolutions.com>
2013-06-01 09:04:23 -0400

    Adds MetaCPAN::Util

---
+++ lib/MetaCPAN/Util.pm
@@ -0,0 +1,17 @@
+package MetaCPAN::Util;
+
+use strict;
+use warnings;
+
+use ElasticSearch;
+use Sub::Exporter -setup => { exports => ['es'] };
+
+sub es {
+    return ElasticSearch->new(
+        no_refresh  => 1,
+        servers     => 'api.metacpan.org',
+        trace_calls => \*STDOUT,
+    );
+}
+
+1;
