309bb9fe19587a6f45f646b096f7a9e85251ebca
Olaf Alders <olaf@wundersolutions.com>
2014-03-13 17:10:45 +0100

    Adds example for how to get all releases for one author.

---
+++ scripts/endpoints/release/all-releases-by-author.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature qw( say );
+
+use DateTime;
+use MetaCPAN::Util qw( es );
+
+my $uploads = es()->search(
+    index => 'v0',
+    type  => 'release',
+    query => {
+        filtered => {
+            query  => { match_all => {} },
+            filter => { term      => { 'release.author' => 'OALDERS' } },
+        },
+    },
+    fields => ['author', 'archive', 'date' ],
+    sort => [{ "date" => "desc" }],
+    size => 100,
+);
+
+my @releases = map { $_->{fields} } @{ $uploads->{hits}->{hits} };
+use DDP;
+p @releases;
