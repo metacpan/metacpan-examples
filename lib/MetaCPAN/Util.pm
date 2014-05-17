424f285cea38098eb409f535957c5b07b69495fe
Talina06 <talina.shrotriya@gmail.com>
2014-05-17 23:34:31 +0530

    changes to Elastic search API.

--- lib/MetaCPAN/Util.pm
+++ lib/MetaCPAN/Util.pm
@@ -3,15 +3,14 @@ package MetaCPAN::Util;
 use strict;
 use warnings;
 
-use ElasticSearch;
+use Search::Elasticsearch;
 use Sub::Exporter -setup => { exports => ['es'] };
 
 sub es {
-    return ElasticSearch->new(
-        no_refresh  => 1,
-        servers     => 'api.metacpan.org',
-        trace_calls => \*STDOUT,
-        transport   => 'curl',
+    return Search::Elasticsearch->new(
+        cxn_pool => "Static::NoPing",
+        nodes     => "api.metacpan.org",
+        trace_to => "Stdout",
     );
 }
 
