987b22785090e6e3c3ccde33335cbe6d3b916b98
Talina06 <talina.shrotriya@gmail.com>
2014-06-11 09:10:20 -0400

    Changes to the author scripts.

--- scripts/endpoints/author/1a-search-authors-mcpan-api.pl
+++ scripts/endpoints/author/1a-search-authors-mcpan-api.pl
@@ -5,25 +5,17 @@ use warnings;
 use feature qw( say );
 
 use Data::Printer;
-use MetaCPAN::API;
+use MetaCPAN::Client;
 
-my $mc = MetaCPAN::API->new;
+my $mc = MetaCPAN::Client->new;
 
-my $search  = $mc->post(
-    'author',
-    {   query => { match_all => {} },
-        size  => 5,
-    },
-);
+my $search = $mc->author( { name => 'Olaf *' } );
 
 say "raw results";
-say '#'x80;
-p $search;
+say '#' x 80;
+while ( my $author = $search->next ) {
+    p $search;
+}
 
-
-say '#'x80;
-say 'Total matches: ' . $search->{hits}->{total};
-
-say "First result";
-say '#'x80;
-p $search->{hits}->{hits}->[0]->{_source};
+say '#' x 80;
+say 'Total matches: ' . $search->total;
