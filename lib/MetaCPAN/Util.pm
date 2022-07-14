9f347c8746552777e7196667725242ea2f575d70
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-07-13 23:26:49 -0400

    Add debug flag $ENV{METACPAN_EXAMPLES_DEBUG}
    This makes the output less verbose by default.

--- lib/MetaCPAN/Util.pm
+++ lib/MetaCPAN/Util.pm
@@ -3,6 +3,8 @@ package MetaCPAN::Util;
 use strict;
 use warnings;
 
+use constant DEBUG => !!$ENV{METACPAN_EXAMPLES_DEBUG};
+
 use Search::Elasticsearch;
 use Sub::Exporter -setup => { exports => ['es'] };
 
@@ -12,8 +14,35 @@ sub es {
         cxn_pool         => 'Static::NoPing',
         nodes            => 'https://fastapi.metacpan.org/v1',
         send_get_body_as => 'POST',
-        trace_to         => 'Stdout',
+        ( trace_to => 'Stdout' ) x !!(DEBUG),
     );
 }
 
 1;
+
+=head1 NAME
+
+MetaCPAN::Util - Utilities for accessing MetaCPAN
+
+=head1 DESCRIPTION
+
+Provides shared utility code for examples.
+
+=head1 FUNCTIONS
+
+=head2 es
+
+Returns a L<Search::Elasticsearch> client configured for use with the
+MetaCPAN API endpoint.
+
+=head1 ENVIRONMENT
+
+=over 4
+
+=item C<METACPAN_EXAMPLES_DEBUG>
+
+When set to C<1>, enables output of verbose debugging information.
+
+=back
+
+=cut
