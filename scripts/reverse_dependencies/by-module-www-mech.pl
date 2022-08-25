26b4f5b4f1f959fc380ed0aa1e28390eaa046fc7
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-08-24 23:17:14 -0400

    Add reverse dependencies example by module

---
+++ scripts/reverse_dependencies/by-module-www-mech.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Data::Printer;
+use JSON::MaybeXS qw( decode_json );
+use WWW::Mechanize::GZip ();
+
+my $mech = WWW::Mechanize::GZip->new;
+
+$mech->get(
+    "https://fastapi.metacpan.org/v1/reverse_dependencies/module/WWW::Mechanize"
+);
+
+my $results = decode_json( $mech->content );
+
+my @dists
+    = map { $_->{distribution} } @{ $results->{data} };
+p @dists;
